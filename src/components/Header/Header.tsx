ff054c495ab80a26bc67d69c86c8f17631a916be
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-27 18:49:24 +0200

    update Header: update menu and logo images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,38 @@
 import style from "./Header.module.css"
+import { useState } from 'react';
 
 const menu = [
-  "Home", "Work", "Contact"
+  "Home", "Work", "Skills", "Contact"
 ]
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
         <div className={style.container}>
             <div className={style.header}>
-              <a href="javascript:void(0)">O.Vovnenko</a>
-              <menu>
-                {menu.map((menu , idx) => <li key={idx}>
+              <a className={style.logo} href="#">
+                <img src="/public/logo.png" alt="logo" />
+                O.Vovnenko
+              </a>
+              <menu className={isOpen ? style.active : undefined}>
+                {menu.map((menu , idx) =>
+                  <li
+                    className={style.item}
+                    key={idx}
+                    onClick={() => setIsOpen(false)}
+                  >
                   {menu}
                 </li>)}
               </menu>
+                <div
+                  className={!isOpen ? style.burger : style.none}
+                  onClick={() => setIsOpen(true)}
+                >
+                  <span></span>
+                <
+                /div>
             </div>
         </div>
     </header>
