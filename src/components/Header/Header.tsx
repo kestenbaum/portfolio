8f9b2c6def58698199c0e742c166d8aacd465ccf
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-27 18:52:15 +0200

    update Header: update menu and logo images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,38 @@
-import style from "./Header.module.css"
 import { useState } from 'react';
+import style from './Header.module.css';
 
-const menu = [
-  "Home", "Work", "Skills", "Contact"
-]
+const menu = ['Home', 'Work', 'Skills', 'Contact'];
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header>
-        <div className={style.container}>
-            <div className={style.header}>
-              <a className={style.logo} href="#">
-                <img src="/public/logo.png" alt="logo" />
-                O.Vovnenko
-              </a>
-              <menu className={isOpen ? style.active : undefined}>
-                {menu.map((menu , idx) =>
-                  <li
-                    className={style.item}
-                    key={idx}
-                    onClick={() => setIsOpen(false)}
-                  >
-                  {menu}
-                </li>)}
-              </menu>
-                <div
-                  className={!isOpen ? style.burger : style.none}
-                  onClick={() => setIsOpen(true)}
-                >
-                  <span></span>
-                <
-                /div>
-            </div>
+      <div className={style.container}>
+        <div className={style.header}>
+          <a className={style.logo} href="#">
+            <img src="/public/logo.png" alt="logo" />
+            O.Vovnenko
+          </a>
+          <menu className={isOpen ? style.active : undefined}>
+            {menu.map((menu, idx) => (
+              <li
+                className={style.item}
+                key={idx}
+                onClick={() => setIsOpen(false)}
+              >
+                {menu}
+              </li>
+            ))}
+          </menu>
+          <div
+            className={!isOpen ? style.burger : style.none}
+            onClick={() => setIsOpen(true)}
+          >
+            <span></span>
+          </div>
         </div>
+      </div>
     </header>
   );
 };
